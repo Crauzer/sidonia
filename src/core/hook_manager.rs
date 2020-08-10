b266d891656a3fd03219b207614022b553bfa241
Crauzer <filip.quitko@gmail.com>
2020-08-10 17:59:00 +0200

    gud code

--- src/core/hook_manager.rs
+++ src/core/hook_manager.rs
@@ -1,4 +1,4 @@
-use crate::core::hook_manager::HookManagerError::HookNotFound;
+use crate::core::hook_manager::HookManagerError::{HookEnableFailed, HookNotFound};
 use detour::{Function, GenericDetour, StaticDetour};
 use std::{
     collections::{hash_map::Entry, HashMap},
@@ -6,21 +6,26 @@ use std::{
     fmt,
     path::Display,
 };
-use winapi::_core::fmt::Formatter;
 
 pub struct HookManager<F: Function> {
     hooks: HashMap<String, GenericDetour<F>>,
 }
 
+#[derive(Debug)]
 pub enum HookManagerError {
     HookAlreadyRegistered(String),
     HookNotFound(String),
+    HookEnableFailed(String, detour::Error),
     HookDisableFailed(String, detour::Error),
 }
 
 impl<F: Function> HookManager<F> {
     pub fn new() -> Self {
         HookManager { hooks: HashMap::new() }
+    }
+
+    pub fn hooks(&self) -> &HashMap<String, GenericDetour<F>> {
+        &self.hooks
     }
 
     pub fn register_hook(&mut self, name: &str, hook: GenericDetour<F>) -> Result<(), HookManagerError> {
@@ -63,24 +68,69 @@ impl<F: Function> HookManager<F> {
         Ok(())
     }
 
-    pub fn initialize_hooks() -> Result<(), HookManagerError> {
+    pub fn enable_hooks(&self) -> Result<(), HookManagerError> {
+        for (hook_name, hook) in self.hooks.iter().filter(|hook_entry| !hook_entry.1.is_enabled()) {
+            unsafe {
+                hook.enable()
+                    .or_else(|err| return Err(HookManagerError::HookEnableFailed(hook_name.clone(), err)));
+            }
+
+            log::info!("Enabled hook: {}", hook_name);
+        }
+
+        Ok(())
+    }
+    pub fn disable_hooks(&self) -> Result<(), HookManagerError> {
+        for (hook_name, hook) in self.hooks.iter().filter(|hook_entry| hook_entry.1.is_enabled()) {
+            unsafe {
+                hook.enable()
+                    .or_else(|err| return Err(HookManagerError::HookDisableFailed(hook_name.clone(), err)));
+            }
+
+            log::info!("Disabled hook {}", hook_name);
+        }
+
         Ok(())
     }
 
-    pub fn enable_hooks() -> Result<(), HookManagerError> {
-        Ok(())
-    }
+    pub fn enable_hook(&self, hook_name: &str) -> Result<(), HookManagerError> {
+        log::info!("Enabling hook: {}", hook_name);
 
-    pub fn disable_hooks() -> Result<(), HookManagerError> {
-        Ok(())
+        // TODO: disgusting pls fix
+        self.hooks
+            .get(hook_name)
+            .map_or(Err(HookManagerError::HookNotFound(String::from(hook_name))), |hook| unsafe {
+                if !hook.is_enabled() {
+                    hook.enable()
+                        .or_else(|err| return Err(HookManagerError::HookEnableFailed(String::from(hook_name), err)))
+                } else {
+                    Ok(())
+                }
+            })
+    }
+    pub fn disable_hook(&self, hook_name: &str) -> Result<(), HookManagerError> {
+        log::info!("Disabling hook: {}", hook_name);
+
+        // TODO: disgusting pls fix
+        self.hooks
+            .get(hook_name)
+            .map_or(Err(HookManagerError::HookNotFound(String::from(hook_name))), |hook| unsafe {
+                if hook.is_enabled() {
+                    hook.disable()
+                        .or_else(|err| return Err(HookManagerError::HookDisableFailed(String::from(hook_name), err)))
+                } else {
+                    Ok(())
+                }
+            })
     }
 }
 
 impl fmt::Display for HookManagerError {
-    fn fmt(&self, f: &mut Formatter) -> fmt::Result {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
         match self {
             HookManagerError::HookAlreadyRegistered(name) => write!(f, "{} is already registered", name),
             HookManagerError::HookNotFound(name) => write!(f, "{} was not found in the map of registered hooks", name),
+            HookManagerError::HookEnableFailed(name, err) => write!(f, "Failed to enable hook {}, reason: {}", name, err),
             HookManagerError::HookDisableFailed(name, err) => write!(f, "Failed to disable hook {}, reason: {}", name, err),
         }
     }
